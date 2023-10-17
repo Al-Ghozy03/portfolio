4950f7b0d60ae0212a25b1ef5f472ca2b324496b
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2023-10-17 13:54:16 +0700

    feat: navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,7 +1,23 @@
 "use client";
+import Image from "next/image";
+import logo from "../assets/img/logo.png";
 
 export default function Navbar() {
-  return <nav>
-    
-  </nav>;
+  return (
+    <header className="flex justify-between items-center">
+      <Image src={logo} alt="logo" height={30} priority />
+      <nav className="capitalize lg:flex lg:space-x-5 lg:text-sm items-center">
+        <p>about</p>
+        <p>skills</p>
+        <p>my services</p>
+        <p>work experiences</p>
+        <p>portfolio</p>
+        <div className="bg-gradient-to-r from-[#834D9B] to-[#D04ED6] p-px flex justify-center items-center rounded-full">
+          <button className="w-full hover:bg-gradient-to-r hover:from-[#834D9B] hover:to-[#D04ED6] hover:font-semibold bg-[#161616] px-5 py-2.5 rounded-full">
+            Contact me
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 "use client";
+import Navbar from "./components/navbar";
+import circle from "./assets/img/circle.png";
+
 export default function Home() {
   const openEmail = () => {
     const mailTo = `mailto:muhammadfaizalghozi1@gmail.com?`;
     window.location.href = mailTo;
   };
   return (
-    <main className="px-10 py-7 w-full">
+    <main className="px-10 py-7 w-full bg-cover">
+      <Navbar />
     </main>
   );
 }
