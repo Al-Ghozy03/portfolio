8d938245c97fb3816ea223679c806142791f1979
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2023-10-17 13:13:12 +0700

    chore:deleting old code

--- src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-export default function Title({ children, className }) {
-  return (
-    <div>
-      <h1 className="text-3xl text-center font-semibold">{children}</h1>
-      <div className={`h-1 ${className} mx-auto bg-[#3F50EA]`}></div>
-    </div>
-  );
-}
