d9258233c03cb4bae01c8f66fd0e7263bc6bb445
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2024-03-13 07:29:45 +0700

    deps: aos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,21 +3,24 @@ import dot from "@/app/assets/img/dot.png";
 import Image from "next/image";
 
 import Landing from "./components/landing";
+import AOS from "aos";
 
 export const openEmail = () => {
   const mailTo = `mailto:muhammadfaizalghozi1@gmail.com?`;
   window.location.href = mailTo;
 };
+AOS.init();
+import "aos/dist/aos.css";
 
 export default function Home() {
   return (
     <main className="pb-7 w-full">
       <div className="px-20 py-20 absolute">
-        <Image src={dot} />
+        <Image src={dot} alt="dot" />
       </div>
       <Landing />
       <div className="px-20 right-0 absolute">
-        <Image src={dot} />
+        <Image src={dot} alt="dot" />
       </div>
     </main>
   );
