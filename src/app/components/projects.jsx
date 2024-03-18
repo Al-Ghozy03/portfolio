ad3f3d204e53c6619134fd454b0f47d525d76a59
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2024-03-18 14:58:54 +0700

    style: bg project size

---
+++ src/app/components/projects.jsx
@@ -0,0 +1,43 @@
+import projects from "../../variable/portfolio";
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="px-20 mt-24">
+      <h1 className="text-4xl font-bold text-right">
+        My <span className="text-[#FF644D]">Projects</span>
+      </h1>
+      <p className="text-xl text-right mt-3 leading-6">
+        See my work experiences and the
+        <br />
+        achievements I have accomplished
+      </p>
+      <div className="grid grid-cols-4 gap-x-10 mt-10">
+        {projects.map((val, k) => (
+          <Card key={k} data={val} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Card({ data }) {
+  return (
+    <div className="border-2 rounded-3xl flex flex-col px-5 py-5 mb-10 bg-white shadow-xl shadow-gray-200">
+      <div
+        className="h-80 w-full rounded-2xl"
+        style={{
+          background: `url(${data.thumbnail.src})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      <a href={data.url} target="_blank">
+        <h2 className="text-2xl font-semibold mt-5 mb-2 hover:underline">
+          {data.title}
+        </h2>
+      </a>
+      <p className="text-sm text-gray-600">{data.description}</p>
+    </div>
+  );
+}
