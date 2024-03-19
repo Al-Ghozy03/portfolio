6025299f24a166be5b90f722710a3e70a4bcb7db
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2024-03-19 08:05:57 +0700

    footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,9 @@
 "use client";
 import dot from "./assets/img/dot.png";
+import logo from "./assets/img/logo.png";
 import Image from "next/image";
 import Landing from "./components/landing";
 
-export const openEmail = () => {
-  const mailTo = `mailto:muhammadfaizalghozi1@gmail.com?`;
-  window.location.href = mailTo;
-};
 import About from "./components/about";
 import Services from "./components/services";
 import Skills from "./components/skills";
@@ -14,9 +11,14 @@ import WorkExperiences from "./components/work_experiences";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
 
+export const openEmail = () => {
+  const mailTo = `mailto:muhammadfaizalghozi1@gmail.com?`;
+  window.location.href = mailTo;
+};
+
 export default function Home() {
   return (
-    <main className="pb-7 w-full">
+    <main className="w-full">
       <div className="px-20 py-20 absolute">
         <Image src={dot} alt="dot" />
       </div>
@@ -45,6 +47,17 @@ export default function Home() {
         <Image src={dot} alt="dot" />
       </div>
       <Contact />
+      <footer className="flex justify-between px-20 bg-[#F7F8F9] mt-20 py-20">
+        <div>
+          <Image src={logo} alt="Muhammad Faiz Al Ghozi" />
+          <p className="mt-1">Illustration from <span className="font-bold"><a target="_blank" href="https://freepik.com">Freepik</a></span></p>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-lg font-semibold">My Contacts</h3>
+          <a target="_blank" href="mailto:muhammadfaizalghozi1@gmail.com">muhammadfaizalghozi1@gmail.com</a>
+          <a target="_blank" href="wa.me/6287818197732">+62 878 1819 7732</a>
+        </div>
+      </footer>
     </main>
   );
 }
